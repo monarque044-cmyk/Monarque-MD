985fe96b094544b575d3ff9762435ea60e15f2db
monarque044-cmyk <diasmael044@gmail.com>
2026-02-26 22:22:45 +0100

    Remove unused axios import
    Removed unused axios import from messageHandler.js

--- events/messageHandler.js
+++ events/messageHandler.js
@@ -31,7 +31,6 @@ import uptime from '../commands/uptime.js'
 import compliment from '../commands/compliment.js'
 import weather from '../commands/weather.js'
 import goodnight from '../commands/goodnight.js'
-import axios from 'axios'
 
 async function handleIncomingMessage(client, event) {
     let lid = client?.user?.lid.split(':')[0] + '@lid'
