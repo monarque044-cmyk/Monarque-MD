52959ff09ca80702ffa58694652c0db35f309f98
monarque044-cmyk <diasmael044@gmail.com>
2026-02-26 13:54:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ import handleIncomingMessage from './events/messageHandler.js'
 })()
 
 const complimentCommand = require('./compliment.js');
+
+// index.js
+const helpCommand = require('./help.js');
+
+// ...
+
+const commandes = [
+  helpCommand,
+  // ajoute d'autres commandes ici
+];
+
+// ...
+
+client.on('message', message => {
+  const cmd = commandes.find(c => c.nom === message.body);
+  if (cmd) {
+    cmd.execute(message, commandes);
+  }
+});
