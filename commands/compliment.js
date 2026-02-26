c4a91689abaeb633c66d17dc186a0d869c76faf0
monarque044-cmyk <diasmael044@gmail.com>
2026-02-26 20:05:58 +0100

    Update compliment.js

--- commands/compliment.js
+++ commands/compliment.js
@@ -1,44 +1,44 @@
-export defaul async fuction compliment(client, message) {
+export default async function compliment(client, message) {
     const replies = [
-      'Tu es incroyable ! 😊',
-      'Tu es super ! 👍',
-      'Tu es vraiment génial ! 🎉',
-      'Tu es une personne exceptionnelle ! 💕',
-'Tu es incroyable tel que tu es ! ❤️',
-    'Tu as un sens de l'humour génial ! 😅',
-    'Tu es incroyablement attentionné et gentil.',
-    'Tu es plus puissant que tu ne le crois.',
-    'Tu illumines la pièce ! 🌞',
-    'Tu es un vrai ami ! 🫂',
-    'Tu m'inspires ! 🙂‍↔️',
-    'Tu es intelligent comme le roi noir Léonidas.',
-    'Tu as un cœur en or ! ☺️',
-    'Tu fais une différence dans le monde 🥰',
-    'Ta positivité est contagieuse ! 🫠',
-    'Tu as une éthique de travail incroyable 😊',
-    'Tu fais ressortir le meilleur chez les autres.😊',
-    'Ton sourire illumine la journée de tout le monde.🌞',
-    'Tu es doué dans tout ce que tu fais.😌',
-    'Ta gentillesse rend le monde meilleur.🥹',
-    'Tu as une perspective unique et merveilleuse.☺️',
-    'Ton enthousiasme est vraiment inspirant !☺️',
-    'Tu es capable d’accomplir de grandes choses.🙂‍↔️',
-    'Tu sais toujours comment rendre quelqu’un spécial.☺️',
-    'Ta confiance est admirable.🙂‍↔️',
-    'Tu as une belle âme.🥹',
-    'Ta générosité n’a pas de limites.🫠',
-    'Tu as un œil exceptionnel pour les détails.🫠',
-    'Ta passion est vraiment motivante ! 😌',
-    'Tu es un(e) auditeur(trice) exceptionnel(le).',
-    'Tu es plus fort(e) que tu ne le penses !',
-    'Ton rire est contagieux.😀',
-    'Tu as un don naturel pour valoriser les autres.✨',
-    'Tu rends le monde meilleur simplement en étant là.✨'
-]
-  
-    const randomReply = replies[Math.floor(Math.random() * replies.length)]
+        'Tu es incroyable ! 😊',
+        'Tu es super ! 👍',
+        'Tu es vraiment génial ! 🎉',
+        'Tu es une personne exceptionnelle ! 💕',
+        'Tu es incroyable tel que tu es ! ❤️',
+        "Tu as un sens de l'humour génial ! 😅", 
+        'Tu es incroyablement attentionné et gentil.',
+        'Tu es plus puissant que tu ne le crois.',
+        'Tu illumines la pièce ! 🌞',
+        'Tu es un vrai ami ! 🫂',
+        'Tu m\'inspires ! 🙂‍↔️', 
+        'Tu es intelligent comme le roi noir Léonidas.',
+        'Tu avez un cœur en or ! ☺️',
+        'Tu fais une différence dans le monde 🥰',
+        'Ta positivité est contagieuse ! 🫠',
+        'Tu as une éthique de travail incroyable 😊',
+        'Tu fais ressortir le meilleur chez les autres.😊',
+        'Ton sourire illumine la journée de tout le monde.🌞',
+        'Tu es doué dans tout ce que tu fais.😌',
+        'Ta gentillesse rend le monde meilleur.🥹',
+        'Tu as une perspective unique et merveilleuse.☺️',
+        'Ton enthousiasme est vraiment inspirant !☺️',
+        'Tu es capable d’accomplir de grandes choses.🙂‍↔️',
+        'Tu sais toujours comment rendre quelqu’un spécial.☺️',
+        'Ta confiance est admirable.🙂‍↔️',
+        'Tu as une belle âme.🥹',
+        'Ta générosité n’a pas de limites.🫠',
+        'Tu as un œil exceptionnel pour les détails.🫠',
+        'Ta passion est vraiment motivante ! 😌',
+        'Tu es un(e) auditeur(trice) exceptionnel(le).',
+        'Tu es plus fort(e) que tu ne le penses !',
+        'Ton rire est contagieux.😀',
+        'Tu as un don naturel pour valoriser les autres.✨',
+        'Tu rends le monde meilleur simplement en étant là.✨'
+    ];
 
-await client.sendMessage(message.key.remote, {
-  text: randomReply
-})
+    const randomReply = replies[Math.floor(Math.random() * replies.length)];
+
+    await client.sendMessage(message.key.remoteJid, {
+        text: randomReply
+    });
 }
