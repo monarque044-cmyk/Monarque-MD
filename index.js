8190705674a416e5a00d444908fa4fe65181f443
monarque044-cmyk <diasmael044@gmail.com>
2026-02-26 16:04:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,3 @@ import handleIncomingMessage from './events/messageHandler.js'
     await connectToWhatsapp(handleIncomingMessage)
         console.log('established !')
 })()
-
-const complimentCommand = require('./compliment.js');
-
-// index.js
-const helpCommand = require('.commands/help.js');
-
-// ...
-
-const command = [
-  helpCommand,
-  // ajoute d'autres commandes ici
-];
-
-// ...
-
-client.on('message', message => {
-  const cmd = commandes.find(c => c.nom === message.body);
-  if (cmd) {
-    cmd.execute(message, commandes);
-  }
-});
-
-const antibotCommand = require('./commands/antibot.js');
