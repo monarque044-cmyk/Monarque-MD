dc7704dd6d671b74873400fb9affd07a683897e9
monarque044-cmyk <diasmael044@gmail.com>
2026-02-26 16:57:57 +0100

    Fix import path for DigixNew in viewonce.js

--- commands/viewonce.js
+++ commands/viewonce.js
@@ -1,4 +1,4 @@
-import { DigixNew } from '../utils/Momokex.js';
+import { DigixNew } from '../utils/MomoKex.js';
 import { downloadMediaMessage } from 'baileys';
 import fs from 'fs';
 import path from 'path';
