9014d1764dc22f5039c59b99789b44032320bf34
monarque044-cmyk <diasmael044@gmail.com>
2026-02-27 20:37:29 +0100

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,7 +7,7 @@ export async function uptime(client, message) {
     const minutes = Math.floor((uptime % 3600) / 60)
     const seconds = Math.floor(uptime % 60)
     
-    const text = `┌─🤖 𝑍ᴇʀ⭕✞︎DIAS 227─┐
+    const text = `┌─🤖 MONARQUE MD─┐
 │
 │ ⏱️ Uptime: ${days}d ${hours}h ${minutes}m
 │ 💾 RAM: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(1)}MB
