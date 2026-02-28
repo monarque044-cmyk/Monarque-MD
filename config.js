977fec4f2b4c2b6113ecedf53500617644c93edd
monarque044-cmyk <diasmael044@gmail.com>
2026-02-28 17:25:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  Number: "22780828646" // remplacez par votre numéro WhatsApp sans mettre le signe +
+const config = {
+    ownerNumber: "22780828646", // Ton numéro sans le +
+    ownerJid: "22780828646@s.whatsapp.net", // Format WhatsApp officiel
+    prefix: ".",
+    publicMode: true
 };
+
+export default config;
